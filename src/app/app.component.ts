3cf3f31a28063e406e72d5c1cfafa881cda90529
leonintime <108757900+leonphem456@users.noreply.github.com>
2023-01-28 16:46:35 +0100

    Big changes + Added pictures + Added ngfor to loop all sliders in div    containers - Removed unnecessary code + Added method that gets books for every genre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+
+export class Book {
+  constructor(
+    public id: number,
+    public title: string,
+    public image: string,
+    public price: number,
+    public thumbImage: string,
+    public genre: Genre,
+    public author: Author
+
+  ) {
+
+  }
+}
+
+export class Genre {
+  constructor(public id: number, public name: string) {
+  }
+}
+
+export class Author {
+  constructor(public id: number, public name: string) {
+  }
+}
+
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'books_ui';
+export class AppComponent implements OnInit {
+
+
+
+  title = 'books';
+
+  bookUrl = "http://localhost:8081/books";
+  genreUrl = "http://localhost:8081/genres";
+
+  books: Book[] = [];
+  genres: Genre[] = [];
+  booksByGenre: Book[][] = [];
+  genreAmount: number = 0;
+  genreCounter: number = 0;
+  bookIterator: number = 0;
+  currentGenre: number = 0;
+  imageArray: any[][] = [];
+
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getAllGenres();
+    this.getAllBooksByGenre();
+  }
+
+
+
+  getAllGenres() {
+    this.httpClient.get<any>(this.genreUrl + "/all-genres").subscribe(response => {
+      this.genres = response;
+    })
+  }
+
+
+
+  getAllBooksByGenre() {
+    console.log(this.booksByGenre);
+    this.httpClient.get<any>(this.genreUrl + "/all-genres").subscribe(response => {
+
+      this.genres = response;
+
+      this.genres.forEach(genre => {
+        this.genreCounter++;
+
+        this.httpClient.get<any>(this.bookUrl + "/findByGenre/" + this.genreCounter).subscribe(response => {
+          this.imageArray[this.bookIterator] = response;
+          this.bookIterator++;
+        })
+
+      });
+
+    })
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { BookListComponent } from './book-list/book-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+ import { NgImageSliderModule } from 'ng-image-slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // BookListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+     NgImageSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
